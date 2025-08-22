3ada10fd8d80c502cbbb7d151051f3572a7e7a8c
N-K-L1 <6631501179@lamduan.mfu.ac.th>
2025-08-22 20:37:00 +0700

    Initialize the backend part for expenses app.

---
+++ serverdb.js
@@ -0,0 +1,92 @@
+const con = require('./db');
+const express = require('express');
+const bcrypt = require('bcrypt');
+const app = express();
+const saltRounds = 10; // Strength of the password hashing
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+
+// password generator
+app.get('/password/:pass', (req, res) => {
+    const password = req.params.pass;
+    bcrypt.hash(password, 10, function(err, hash) {
+        if(err) {
+            return res.status(500).send('Hashing error');
+        }
+        res.send(hash);
+    });
+});
+
+
+// login
+app.post('/login', (req, res) => {
+    const {username, password} = req.body;
+    const sql = "SELECT id, password FROM users WHERE username = ?";
+    con.query(sql, [username], function(err, results) {
+        if(err) {
+            return res.status(500).send("Database server error");
+        }
+        if(results.length != 1) {
+            return res.status(401).send("Wrong username");
+        }
+        // compare passwords
+        bcrypt.compare(password, results[0].password, function(err, same) {
+            if(err) {
+                return res.status(500).send("Hashing error");
+            }
+            if(same) {
+                 return res.json({ message: "Login OK", userId: results[0].id });
+            }
+            return res.status(401).send("Wrong password");
+        });
+    })
+});
+
+
+// Show all expenses for a given user
+app.get('/expenses/:userId', (req, res) => {
+    const userId = req.params.userId;
+    const sql = "SELECT * FROM expense WHERE user_id = ?";
+    con.query(sql, [userId], function(err, results) {
+        if (err) {
+            return res.status(500).send("Database server error");
+        }
+        res.json(results);
+    });
+});
+
+// Show today's expenses for a given user
+app.get('/expenses/:userId/today', (req, res) => {
+    const userId = req.params.userId;
+    const sql = "SELECT * FROM expense WHERE user_id = ? AND DATE(date) = CURDATE()";
+    con.query(sql, [userId], function(err, results) {
+        if (err) {
+            return res.status(500).send("Database server error");
+        }
+        res.json(results);
+    });
+});
+
+// //Get the result based on user's search
+// app.get('/expenses/search', (req, res) => {
+
+//     });
+
+// //Add new expenses for a given user
+// app.post('/expenses/add', (req, res) => {
+
+//     });
+
+// //Delete recorded expenses for a given user    
+// app.delete('/expenses/delete', (req, res) => {
+
+//     });
+
+
+// ---------- Server starts here ---------
+const PORT = 3000;
+app.listen(PORT, () => {
+    console.log('Server is running at ' + PORT);
+});
